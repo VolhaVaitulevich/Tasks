0803f0b8bbf61d3f859c9303b4194c4bc520af54
OlgaV <o.voytulevich@gmail.com>
2022-10-10 21:31:58 +0200

    remove task 2 from secondLargestNumber branch

--- exercises/arrays/lodashCompact/index.ts
+++
@@ -1,6 +0,0 @@
-const arrTask2: any[] = [0, 1, false, 2, "", 3];
-
-const compact = (arrTask2: any[]): any => arrTask2.filter(Boolean);
-//return arr2.filter((value) => value);
-
-console.log(`Task 2: ${compact(arrTask2)}`);
