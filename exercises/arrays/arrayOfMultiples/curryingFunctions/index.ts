817dcc026c12b6da309096ff988f2f1aec7922ff
OlgaV <o.voytulevich@gmail.com>
2022-10-10 21:37:57 +0200

    add task 4 - Currying Functions

---
+++ exercises/arrays/arrayOfMultiples/curryingFunctions/index.ts
@@ -0,0 +1,9 @@
+const arrTask4: number[] = [4, 6, 5];
+const a: number = 10;
+
+const multiply =
+  (arrTask4: number[]) =>
+  (a: number): number[] =>
+    arrTask4.map((item) => item * a);
+
+console.log(`Task 4: ${multiply(arrTask4)(a)}`);
