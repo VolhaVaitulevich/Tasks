a61ed9378f4f6f92df8c351b31f8e33e6f50178e
OlgaV <o.voytulevich@gmail.com>
2022-10-10 21:59:03 +0200

    remove test console.log

--- exercises/arrays/removeDuplicates/index.ts
+++ exercises/arrays/removeDuplicates/index.ts
@@ -3,5 +3,3 @@ const arrTask6 = [1, 0, 1, 0];
 const removeDups = (arrTask6: any[]): any[] => Array.from(new Set(arrTask6));
 
 console.log(`Task 6: ${removeDups(arrTask6)}`);
-
-console.log("for git test");
