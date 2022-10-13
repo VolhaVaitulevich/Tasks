9d747d0fd341a6694708371384d513ca918da880
OlgaV <o.voytulevich@gmail.com>
2022-10-13 16:23:23 +0200

    update function and correct data type

--- exercises/arrays/ifValuesTrue/index.ts
+++ exercises/arrays/ifValuesTrue/index.ts
@@ -1,6 +1,5 @@
-const allTruthy1 = (...args: number[]): boolean => {
-  let arrTask5 = Array.from([...args], (x: number): number => x);
-  return arrTask5.every(Boolean);
+const allTruthy1 = (...args: (number | string)[]): boolean => {
+  return [...args].every(Boolean);
 };
 
 console.log(`Task 5: ${allTruthy1(5, 4, 3, 2, 1, 0)}`);
