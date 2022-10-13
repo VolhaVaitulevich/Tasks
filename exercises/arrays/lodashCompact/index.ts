2128ee422d31792f31f07738e63c2302c8294526
OlgaV <o.voytulevich@gmail.com>
2022-10-13 16:17:13 +0200

    add new data type for array with multiple types

---
+++ exercises/arrays/lodashCompact/index.ts
@@ -0,0 +1,10 @@
+<<<<<<< HEAD
+=======
+type MyArrayType = number | string | undefined | boolean | null;
+const arrTask2: MyArrayType[] = [0, 1, false, 2, "", 3];
+
+const compact = (arrTask2: MyArrayType[]): MyArrayType[] =>
+  arrTask2.filter(Boolean);
+
+console.log(`Task 2: ${compact(arrTask2)}`);
+>>>>>>> c66d6a1 (add new data type for array with multiple types)
