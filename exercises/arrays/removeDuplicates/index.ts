a19a587fe0c63d1e148858135bb72c76d256cdce
OlgaV <o.voytulevich@gmail.com>
2022-10-13 16:28:51 +0200

    add data type for array

--- exercises/arrays/removeDuplicates/index.ts
+++ exercises/arrays/removeDuplicates/index.ts
@@ -1,5 +1,7 @@
-const arrTask6 = [1, 0, 1, 0];
+type MyArrayType = number | string | undefined | null | boolean;
+const arrTask6: MyArrayType[] = [1, 0, 1, 0];
 
-const removeDups = (arrTask6: any[]): any[] => Array.from(new Set(arrTask6));
+const removeDups = (arrTask6: MyArrayType[]): MyArrayType[] =>
+  Array.from(new Set(arrTask6));
 
 console.log(`Task 6: ${removeDups(arrTask6)}`);
