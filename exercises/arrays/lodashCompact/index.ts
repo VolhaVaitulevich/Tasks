060cd2afc2689873bc96e7422a4f8ba438ccbc95
OlgaV <o.voytulevich@gmail.com>
2022-10-14 14:18:09 +0200

    add generics

--- exercises/arrays/lodashCompact/index.ts
+++ exercises/arrays/lodashCompact/index.ts
@@ -1,7 +1,5 @@
-type MyArrayType = number | string | undefined | boolean | null;
-const arrTask2: MyArrayType[] = [0, 1, false, 2, "", 3];
+const arrTask2 = [0, 1, false, 2, "", 3];
 
-const compact = (arrTask2: MyArrayType[]): MyArrayType[] =>
-  arrTask2.filter(Boolean);
+const compact = <T>(arrTask2: T[]): T[] => arrTask2.filter(Boolean);
 
 console.log(`Task 2: ${compact(arrTask2)}`);
