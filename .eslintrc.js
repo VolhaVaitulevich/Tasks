8f85a121d421607b9cf1b65873269204925d535b
OlgaV <o.voytulevich@gmail.com>
2022-10-14 15:26:29 +0200

    Add eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,10 @@ module.exports = {
 	},
 	extends: [
 		'eslint:recommended',
+<<<<<<< HEAD
+=======
+		'plugin:react/recommended',
+>>>>>>> 30b3946 (Add eslint and prettier)
 		'plugin:@typescript-eslint/recommended',
 		'prettier',
 	],
@@ -16,15 +20,22 @@ module.exports = {
 		sourceType: 'module',
 	},
 <<<<<<< HEAD
+<<<<<<< HEAD
 	plugins: [],
 =======
 	plugins: ['react', '@typescript-eslint'],
 >>>>>>> 2fb7ef7 (Add eslint and prettier)
+=======
+	plugins: ['react', '@typescript-eslint'],
+>>>>>>> 30b3946 (Add eslint and prettier)
 	rules: {
 		indent: ['error', 'tab'],
 		'linebreak-style': ['error', 'windows'],
 		quotes: ['error', 'single'],
 		semi: ['error', 'always'],
+<<<<<<< HEAD
 		'@typescript-eslint/explicit-function-return-type': 'error',
+=======
+>>>>>>> 30b3946 (Add eslint and prettier)
 	},
 };
