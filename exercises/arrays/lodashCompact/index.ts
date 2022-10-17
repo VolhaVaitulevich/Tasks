80eeec424f4e1620da77b0573010ae518edfc60d
OlgaV <o.voytulevich@gmail.com>
2022-10-17 12:50:06 +0200

    add FalsyValues type

--- exercises/arrays/lodashCompact/index.ts
+++ exercises/arrays/lodashCompact/index.ts
@@ -1,5 +1,8 @@
+type FalsyValues = 0 | null | false;
+
 const arrTask2 = [0, 1, false, 2, "", 3];
 
-const compact = <T>(arrTask2: T[]): T[] => arrTask2.filter(Boolean);
+const compact = <T>(arrTask2: (T | FalsyValues)[]): T[] =>
+  arrTask2.filter((value): value is T => Boolean(value));
 
 console.log(`Task 2: ${compact(arrTask2)}`);
