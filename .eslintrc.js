973e9c0958b1bb3fab9b287923fdc0ce8f89ef0d
OlgaV <o.voytulevich@gmail.com>
2022-10-17 14:22:49 +0200

    Add explicit-function-return-type for .ts files

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,22 +9,25 @@ module.exports = {
 		'plugin:@typescript-eslint/recommended',
 		'prettier',
 	],
-	overrides: [],
+	overrides: [
+		{
+			files: ['*.ts', '*.tsx'],
+			rules: {
+				'@typescript-eslint/explicit-function-return-type': ['error'],
+			},
+		},
+	],
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		ecmaVersion: 'latest',
 		sourceType: 'module',
 	},
-<<<<<<< HEAD
 	plugins: [],
-=======
-	plugins: ['react', '@typescript-eslint'],
->>>>>>> 2fb7ef7 (Add eslint and prettier)
 	rules: {
 		indent: ['error', 'tab'],
 		'linebreak-style': ['error', 'windows'],
 		quotes: ['error', 'single'],
 		semi: ['error', 'always'],
-		'@typescript-eslint/explicit-function-return-type': 'error',
+		'@typescript-eslint/explicit-function-return-type': 'off',
 	},
 };
