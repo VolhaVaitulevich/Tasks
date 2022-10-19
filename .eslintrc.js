2e8a82f18b53107650b964db306e3fd1174ef3d3
OlgaV <o.voytulevich@gmail.com>
2022-10-19 19:27:10 +0200

    Update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,29 +9,7 @@ module.exports = {
 		'plugin:@typescript-eslint/recommended',
 		'prettier',
 	],
-<<<<<<< HEAD
-<<<<<<< HEAD
-	overrides: [
-		{
-			files: ['*.ts', '*.tsx'],
-			rules: {
-				'@typescript-eslint/explicit-function-return-type': ['error'],
-			},
-		},
-	],
-=======
 	overrides: [],
->>>>>>> 4722547 (Update eslint)
-=======
-	overrides: [
-		{
-			"files": ["*.ts", "*.tsx"],
-			"rules": {				
-				"@typescript-eslint/explicit-function-return-type": ["error"]
-			}
-		}
-	],
->>>>>>> 9f53765 (Add explicit-function-return-type for .ts files)
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		ecmaVersion: 'latest',
@@ -43,14 +21,6 @@ module.exports = {
 		'linebreak-style': ['error', 'windows'],
 		quotes: ['error', 'single'],
 		semi: ['error', 'always'],
-<<<<<<< HEAD
-<<<<<<< HEAD
-		'@typescript-eslint/explicit-function-return-type': 'off',
-=======
 		'@typescript-eslint/explicit-function-return-type': 'error',
->>>>>>> 4722547 (Update eslint)
-=======
-		"@typescript-eslint/explicit-function-return-type": "off"
->>>>>>> 9f53765 (Add explicit-function-return-type for .ts files)
 	},
 };
