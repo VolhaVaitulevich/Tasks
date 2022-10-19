c926da8d9de7b84d4719c8e18d2d140ce5bcda27
OlgaV <o.voytulevich@gmail.com>
2022-10-19 19:27:10 +0200

    Update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,14 +9,7 @@ module.exports = {
 		'plugin:@typescript-eslint/recommended',
 		'prettier',
 	],
-	overrides: [
-		{
-			"files": ["*.ts", "*.tsx"],
-			"rules": {				
-				"@typescript-eslint/explicit-function-return-type": ["error"]
-			}
-		}
-	],
+	overrides: [],
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		ecmaVersion: 'latest',
@@ -36,9 +29,6 @@ module.exports = {
 		'linebreak-style': ['error', 'windows'],
 		quotes: ['error', 'single'],
 		semi: ['error', 'always'],
-<<<<<<< HEAD
 		'@typescript-eslint/explicit-function-return-type': 'error',
-=======
->>>>>>> 30b3946 (Add eslint and prettier)
 	},
 };
