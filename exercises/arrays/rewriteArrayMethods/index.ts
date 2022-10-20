987b49427ea3ce94282fef97d55300abb5ac0e47
OlgaV <o.voytulevich@gmail.com>
2022-10-20 22:56:56 +0200

    Update after wrong merge

--- exercises/arrays/rewriteArrayMethods/index.ts
+++ exercises/arrays/rewriteArrayMethods/index.ts
@@ -1,16 +1,28 @@
-const myMap = <T>(arr: T[], callback: Function): T[] => {
-  const resultArrMap = [];
-  for (let i = 0; i < arr.length; i++) {
-    const newItem = callback(arr[i], i, arr);
-    resultArrMap.push(newItem);
-  }
-  return resultArrMap;
+const myMap = <TInput, TOutput>(
+	arr: TInput[],
+	callback: (item: TInput, index: number, arr: TInput[]) => TOutput,
+): TOutput[] => {
+	const resultArrMap = [];
+	for (let i = 0; i < arr.length; i++) {
+		const newItem = callback(arr[i], i, arr);
+		resultArrMap.push(newItem);
+	}
+	return resultArrMap;
 };
 
-const myReduce = <T>(arr: T[], callback: Function, initialValue: T): T => {
-  let sum = initialValue;
-  for (let i = 0; i < arr.length; i++) {
-    sum = callback(sum, arr[i], i, arr);
-  }
-  return sum;
+const myReduce = <TInput, TOutput>(
+	arr: TInput[],
+	callback: (
+		acc: TOutput,
+		item: TInput,
+		index: number,
+		arr: TInput[],
+	) => TOutput,
+	initialValue: TOutput,
+): TOutput => {
+	let sum = initialValue;
+	for (let i = 0; i < arr.length; i++) {
+		sum = callback(sum, arr[i], i, arr);
+	}
+	return sum;
 };
