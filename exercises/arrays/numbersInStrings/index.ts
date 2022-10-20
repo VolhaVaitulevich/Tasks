c8d74f1cf2a328544420a2b1e5f6794d4bca5a73
OlgaV <o.voytulevich@gmail.com>
2022-10-20 22:24:25 +0200

    Remove wrong folders (after rebasing)

--- exercises/arrays/numbersInStrings/index.ts
+++
@@ -1,7 +0,0 @@
-const arrOfStrings = ["abc", "ab10c", "a10bc", "bcd"];
-
-const numInStr = (arrOfStrings: string[]): string[] => {
-  return arrOfStrings.filter((string) => string.match(/\d+/g));
-};
-
-console.log(numInStr(arrOfStrings));
