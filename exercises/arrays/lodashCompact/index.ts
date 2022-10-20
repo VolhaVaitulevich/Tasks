77b96f13f1b848ebd9046f67ccbdb71568d7906a
OlgaV <o.voytulevich@gmail.com>
2022-10-20 22:24:25 +0200

    Remove wrong folders (after rebasing)

--- exercises/arrays/lodashCompact/index.ts
+++
@@ -1,8 +0,0 @@
-type FalsyValues = undefined | null | 0 | '' | false;
-
-const arrTask2 = [0, 1, false, 2, '', 3];
-
-const compact = <T>(arrTask2: (T | FalsyValues)[]): T[] =>
-	arrTask2.filter((value): value is T => Boolean(value));
-
-console.log(`Task 2: ${compact(arrTask2)}`);
