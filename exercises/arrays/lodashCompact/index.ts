432830d65102e33641255d96e0286166b82e04a5
OlgaV <o.voytulevich@gmail.com>
2022-10-21 13:33:41 +0200

    Updated with eslint

--- exercises/arrays/lodashCompact/index.ts
+++ exercises/arrays/lodashCompact/index.ts
@@ -1,8 +1,8 @@
-type FalsyValues = undefined | null | 0 | "" | false;
+type FalsyValues = undefined | null | 0 | '' | false;
 
-const arrTask2 = [0, 1, false, , -0, 2, "", 3];
+const arrTask2 = [0, 1, false, -0, 2, '', 3];
 
 const compact = <T>(arrTask2: (T | FalsyValues)[]): T[] =>
-  arrTask2.filter((value): value is T => Boolean(value));
+	arrTask2.filter((value): value is T => Boolean(value));
 
 console.log(`Task 2: ${compact(arrTask2)}`);
