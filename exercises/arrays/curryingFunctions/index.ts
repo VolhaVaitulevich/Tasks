24756277e974498ad7249183172939f4607b553e
VolhaVaitulevich <112556535+VolhaVaitulevich@users.noreply.github.com>
2022-10-23 15:52:08 +0200

    add task 2 - Learn Lodash: Compact (#6)
    * add task 2 - Learn Lodash: Compact
    
    * add new data type for array with multiple types
    
    * add generics
    
    * Add eslint and prettier
    
    * add FalsyValues type
    
    * Add explicit-function-return-type for .ts files
    
    * Removed react plugin
    
    * Update FalsyValues
    
    * Update for FalsyValues type
    
    * Update FalsyValues
    
    * Rewrite methods 'reduce' and 'map'
    
    * Update eslint
    
    * add task 2 - Learn Lodash: Compact
    
    * add new data type for array with multiple types
    
    * add generics
    
    * Add eslint and prettier
    
    * Add explicit-function-return-type for .ts files
    
    * Removed react plugin
    
    * Update eslint
    
    * Update types for input and output parameters
    
    * add task 2 - Learn Lodash: Compact
    
    * add new data type for array with multiple types
    
    * add generics
    
    * Add eslint and prettier
    
    * Add explicit-function-return-type for .ts files
    
    * Removed react plugin
    
    * Update eslint
    
    * Remove wrong folders (after rebasing)
    
    * add task 3 - Second largest number (#7)
    
    * add task 4 - Currying Functions (#11)
    
    * add task 6 - Remove duplicates from an array (#10)
    
    * add task 6 - Remove duplicates from an array
    
    * remove test console.log
    
    * add data type for array
    
    * add generics
    
    * Add task - Number of Occurrences (#13)
    
    * Add task - Number of Occurrences
    
    * Change Interfaces to Record
    
    * add task 5 - Check if all values are true (#9)
    
    * add task 5 - Check if all values are true
    
    * update function and correct data type
    
    * add generics
    
    * fix bug with 'args' array
    
    * Add task - Number in Strings (#15)
    
    * Rewrite methods 'reduce' and 'map'
    
    * Update types for input and output parameters
    
    * Add eslint and prettier
    
    * Add explicit-function-return-type for .ts files
    
    * Removed react plugin
    
    * Update eslint
    
    * Add eslint and prettier
    
    * Add explicit-function-return-type for .ts files
    
    * Removed react plugin
    
    * Update eslint
    
    * Remove wrong folders (after rebasing)
    
    * Revert "Update FalsyValues"
    
    This reverts commit c4d45b7bbcb84f834240d9546c61b0a587cf7654.
    
    * Updated with eslint
    
    * Revert "Update FalsyValues"
    
    This reverts commit c4d45b7bbcb84f834240d9546c61b0a587cf7654.

--- exercises/arrays/curryingFunctions/index.ts
+++
@@ -1,9 +0,0 @@
-const arrTask4: number[] = [4, 6, 5];
-const a = 10;
-
-const multiply =
-	(arrTask4: number[]) =>
-	(a: number): number[] =>
-		arrTask4.map(item => item * a);
-
-console.log(`Task 4: ${multiply(arrTask4)(a)}`);
--- exercises/arrays/secondLargestNumber/index.ts
+++
@@ -1,7 +0,0 @@
-const arrTask3: number[] = [25, 143, 89, 13, 105];
-
-const secondLargest = (arrTask3: number[]): number => {
-	return arrTask3.sort((a, b) => a - b)[arrTask3.length - 2];
-};
-
-console.log(`Task 3: ${secondLargest(arrTask3)}`);
