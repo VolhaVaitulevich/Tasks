04caaa20903766ba7bc21c23fc78df316e13bbcd
OlgaV <o.voytulevich@gmail.com>
2022-10-23 22:01:36 +0200

    Add validation for obj, change forEach() to some()

--- exercises/arrays/lodashGet/index.ts
+++ exercises/arrays/lodashGet/index.ts
@@ -5,18 +5,23 @@ const get = (
 	obj: InputObjType,
 	pathStr: string | string[],
 	defaultValue?: string,
-): InputObjType => {
+): InputObjType | string => {
+	if (obj === null || typeof obj !== 'object') {
+		return defaultValue ? defaultValue : 'default';
+	}
 	let newPathStr: string[] = [];
 	if (!Array.isArray(pathStr)) {
 		newPathStr = pathStr.replace(/\[/g, '.').replace(/\]./g, '.').split('.');
 	} else {
 		newPathStr = newPathStr.concat(pathStr);
 	}
-	newPathStr.forEach(item => {
-		if (obj[item] === undefined || obj[item] === null) {
-			return defaultValue ? defaultValue : 'default';
-		} else obj = obj[item];
-	});
-
-	return obj;
+	if (
+		newPathStr.some(item => {
+			if (obj[item] === undefined || obj[item] === null) {
+				return true;
+			} else obj = obj[item];
+		})
+	) {
+		return defaultValue ? defaultValue : 'default';
+	} else return obj;
 };
