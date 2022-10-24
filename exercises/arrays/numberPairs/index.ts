1c89f0ad76d44c8f75323e7a4537a2faec845bd5
OlgaV <o.voytulevich@gmail.com>
2022-10-24 15:35:23 +0200

    Change 'for...in' to 'reduce', remove nested loops

--- exercises/arrays/numberPairs/index.ts
+++ exercises/arrays/numberPairs/index.ts
@@ -2,17 +2,20 @@ const numberPairs = (str: string): number => {
 	const arrOfNumbers = str.split(' ').slice(1);
 	const objOfDuplicates = arrOfNumbers.reduce(
 		(acc: Record<string, number>, item): Record<string, number> => {
-			acc[item] = +arrOfNumbers.filter(num => num === item).length;
+			console.log(acc);
+			if (Object.prototype.hasOwnProperty.call(acc, item)) {
+				acc[item] += 1;
+			} else {
+				acc[item] = 1;
+			}
 			return acc;
 		},
 		{},
 	);
-	console.log(objOfDuplicates);
-	let numberOfPairs = 0;
-	for (const item in objOfDuplicates) {
-		numberOfPairs += Math.floor(objOfDuplicates[item] / 2);
-	}
-	return numberOfPairs;
+	return Object.values(objOfDuplicates).reduce(
+		(acc, item) => (acc += Math.floor(item / 2)),
+		0,
+	);
 };
 
 console.log(numberPairs('7 1 2 1 2 1 3 2'));
