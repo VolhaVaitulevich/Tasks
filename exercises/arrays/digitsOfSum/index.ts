fbabe04f24a7873cbcf3533fce7a765a006ef9e5
OlgaV <o.voytulevich@gmail.com>
2022-11-17 14:01:05 +0100

    Upgrade for recursive version

--- exercises/arrays/digitsOfSum/index.ts
+++ exercises/arrays/digitsOfSum/index.ts
@@ -1,16 +1,18 @@
 const sumDigProd = (...args: number[]): number => {
-	let periodOfArgs = args.reduce((acc: number, item: number): number => {
-		acc = acc + item;
-		return acc;
-	}, 0);
-	while (periodOfArgs > 9) {
-		periodOfArgs = Array.from(String(periodOfArgs), Number).reduce(
-			(acc: number, item: number): number => {
-				acc = acc * item;
-				return acc;
-			},
-			1,
-		);
-	}
-	return periodOfArgs;
+	return periodOfDigits(
+		args.reduce((acc, item) => {
+			acc = acc + item;
+			return acc;
+		}, 0),
+	);
+};
+const periodOfDigits = (periodOfArgs: number): number => {
+	return periodOfArgs < 10
+		? periodOfArgs
+		: periodOfDigits(
+				Array.from(String(periodOfArgs), Number).reduce((acc, item) => {
+					acc = acc * item;
+					return acc;
+				}, 1),
+		  );
 };
