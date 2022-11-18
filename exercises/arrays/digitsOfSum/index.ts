6aa4ae1126d578042e4c13f3a5f684341f0bb7d7
OlgaV <o.voytulevich@gmail.com>
2022-11-18 11:38:42 +0100

    Update reduce methods

--- exercises/arrays/digitsOfSum/index.ts
+++ exercises/arrays/digitsOfSum/index.ts
@@ -1,17 +1,9 @@
 const sumDigProd = (...args: number[]): number => {
-	const periodOfArgs = args.reduce((acc, item) => {
-		acc = acc + item;
-		return acc;
-	}, 0);
+	const periodOfArgs = args.reduce((acc, item) => acc + item, 0);
 	if (periodOfArgs < 10) {
 		return periodOfArgs;
 	} else {
 		const periodOfArgsArr = Array.from(String(periodOfArgs), Number);
-		return sumDigProd(
-			periodOfArgsArr.reduce((acc, item) => {
-				acc = acc * item;
-				return acc;
-			}, 1),
-		);
+		return sumDigProd(periodOfArgsArr.reduce((acc, item) => acc * item, 1));
 	}
 };
