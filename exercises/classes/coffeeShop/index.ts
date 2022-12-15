efc1a5ade8dbe5f3d156d7f44b13bb880c3ee003
VolhaVaitulevich <o.voytulevich@gmail.com>
2022-12-15 20:43:42 +0100

    Add task CoffeeShop

---
+++ exercises/classes/coffeeShop/index.ts
@@ -0,0 +1,114 @@
+interface IMenu {
+	item: string;
+	type: string;
+	price: number;
+}
+
+class CoffeeShop {
+	name: string;
+	menu: Array<[string, string, number]>;
+	orders: Array<String>;
+
+	constructor(
+		name: string,
+		menu: Array<[string, string, number]>,
+		orders: Array<String>,
+	) {
+		this.name = name;
+		this.menu = JSON.parse(JSON.stringify(menu));
+		this.orders = [...orders];
+		this.addOrder = this.addOrder.bind(this);
+		this.fulfillOrder = this.fulfillOrder.bind(this);
+		this.listOrders = this.listOrders.bind(this);
+		this.dueAmount = this.dueAmount.bind(this);
+		this.cheapestItem = this.cheapestItem.bind(this);
+		this.drinksOnly = this.drinksOnly.bind(this);
+		this.foodOnly = this.foodOnly.bind(this);
+		this.createMenuArrOfObj = this.createMenuArrOfObj.bind(this);
+	}
+
+	addOrder(orderName: string) {
+		const menuArrOfObj = this.createMenuArrOfObj();
+
+		//check if this item exists in menu
+		if (menuArrOfObj.find(menuObject => menuObject.item === orderName)) {
+			this.orders.push(orderName);
+			console.log('Order added');
+		} else {
+			console.log('This item is currently unavailable!');
+		}
+	}
+
+	fulfillOrder() {
+		if (this.orders.length === 0) {
+			console.log('All orders have been fulfilled!');
+		} else {
+			console.log(`The ${this.orders.shift()} have been fulfilled`);
+		}
+	}
+
+	listOrders() {
+		console.log(this.orders);
+	}
+
+	dueAmount() {
+		const menuArrOfObj = this.createMenuArrOfObj();
+
+		console.log(
+			Math.round(
+				this.orders.reduce((totalOrderPrice, orderItem) => {
+					return (totalOrderPrice +=
+						menuArrOfObj.find(menuObject => menuObject.item === orderItem)
+							?.price ?? 0);
+				}, 0),
+			),
+		);
+	}
+
+	cheapestItem() {
+		const menuArrOfObj = this.createMenuArrOfObj();
+
+		//find minimal praice among all menu items
+		const minPrice = menuArrOfObj.reduce(
+			(min, menuObject) => (menuObject.price < min ? menuObject.price : min),
+			menuArrOfObj[0].price,
+		);
+		console.log(
+			menuArrOfObj.find(menuObject => menuObject.price === minPrice)?.item,
+		);
+	}
+
+	drinksOnly() {
+		const menuArrOfObj = this.createMenuArrOfObj();
+		console.log(
+			menuArrOfObj
+				.filter(menuObject => {
+					if (menuObject.type === 'drink') {
+						return menuObject.item;
+					}
+				})
+				.map(menuObject => menuObject.item),
+		);
+	}
+
+	foodOnly() {
+		const menuArrOfObj = this.createMenuArrOfObj();
+		console.log(
+			menuArrOfObj
+				.filter(menuObject => {
+					if (menuObject.type === 'food') {
+						return menuObject.item;
+					}
+				})
+				.map(menuObject => menuObject.item),
+		);
+	}
+
+	createMenuArrOfObj() {
+		return this.menu.map((menuItem: [string, string, number]) => ({
+			item: menuItem[0],
+			type: menuItem[1],
+			price: menuItem[2],
+		}));
+	}
+}
